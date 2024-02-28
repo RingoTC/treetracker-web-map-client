eb9e78200e2b5053aad7684fdeb7eef7e32f6fd8
ringotc <ringotc@outlook.com>
2024-02-28 00:00:27 -0800

    feat: test for channel

--- src/pages/404.js
+++ src/pages/404.js
@@ -10,7 +10,7 @@ const pageNotfound = () => (
       <h3>Looks like you&apos;re lost</h3>
       <p>Page you are looking for is missing.</p>
       <Button component={Link} variant="contained" href="/" color="primary">
-        Go home
+        Go home 111
       </Button>
     </div>
   </>
