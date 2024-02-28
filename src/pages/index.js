969f71956d7e73f3faaa040ff26b07f40ca69c3d
ringotc <ringotc@outlook.com>
2024-02-28 00:07:01 -0800

    feat: test for dev

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ export default function Homepage({ nextExtraIsEmbed }) {
         <meta name="author" content="metatags generator" />
         <meta name="revisit-after" content="3 month" />
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
-        <title>Treetracker by Greenstand</title>
+        <title>Treetracker by Greenstand !!!</title>
       </Head>
       <Home />
     </>
